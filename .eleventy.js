5444cb5d7a79cca2cd2eaefb3447aed94e8fa88e
Matt Griffiths <mgriff1984@gmail.com>
2020-11-20 23:54:20 +0000

    Connect Contentful and pull in profile

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 module.exports = (function(eleventyConfig){
-	
+	eleventyConfig.addShortcode('lowercase', function(textString){
+		return textString.toLowerCase();
+	});
 	
 	return {
 		dir: {
---
+++ _input/_data/profile.js
@@ -0,0 +1,16 @@
+const contentful = require("contentful");
+const client = contentful.createClient({
+	space: process.env.CTFL_SPACE,
+	accessToken: process.env.CTFL_ACCESSTOKEN
+});
+
+module.exports = function() {
+	return client.getEntries({
+		content_type: 'profile'
+	})
+	.then((response) => {
+		console.log(JSON.stringify(response));
+		return response.items[0].fields;
+	})
+	.catch(console.error);
+};
